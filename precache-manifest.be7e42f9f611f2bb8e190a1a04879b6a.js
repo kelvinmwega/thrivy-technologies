83d28900710ca35109657de6fdb19e8684e84d48
vunyx <kmwega@gmail.com>
2020-02-04 00:27:09 +0300

    Updates

---
+++ precache-manifest.be7e42f9f611f2bb8e190a1a04879b6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3e057041c48524317d207efd45789c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b2d8edda77f51e65fdb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cf56aa7e57d713bd7dd9",
+    "url": "/static/js/2.ad662863.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ad662863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2d8edda77f51e65fdb",
+    "url": "/static/js/main.16b5baf4.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "df6ea45ef10e0d914387195723273b36",
+    "url": "/static/media/profile.df6ea45e.jpg"
+  }
+]);
